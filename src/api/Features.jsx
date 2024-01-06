7ba9ef3f07bcca6c1b351404f9e5bde71d515c75
Al- Amin <alamin496832@gmail.com>
2024-01-06 22:12:12 +0600

    search api created

--- src/api/Features.jsx
+++ src/api/Features.jsx
@@ -7,6 +7,17 @@ export const getAllFeatures = async () => {
       return response;
     } catch (error) {
       console.error(error.response.data);
-      throw error.response?.data?.msg;
+      throw error.response?.data;
     }
+  };
+
+// Function to search features by query
+export const searchFeatures = async (query) => {
+    try {
+        const response = await axiosOpen.get(`/api/v1/features/search?query=${query}`);
+        return response;
+      } catch (error) {
+        console.log(error)
+        throw error;
+      }
   };
