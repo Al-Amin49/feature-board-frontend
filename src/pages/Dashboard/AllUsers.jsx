33bdde4e4776430b5b6a775f18ead1b2342b847d
Al- Amin <alamin496832@gmail.com>
2024-01-15 00:51:47 +0600

    get all users structure done in dashboard

--- src/pages/Dashboard/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { getAllUsers } from "../../api/User";
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
 
 
 const AllUsers = () => {
     const [users, setUsers]= useState([]);
+    
 
     const fetchAllUsersData = async () => {
         try {
@@ -19,10 +21,62 @@ const AllUsers = () => {
     useEffect(()=>{
         fetchAllUsersData()
     }, [])
+
+    const handleMakeAdmin=()=>{
+
+    }
+    const handleDeleteUser=()=>{
+
+    }
     return (
         <div>
-            <h3>all users</h3>
-            <h4>{users.length}</h4>
+             <div className="flex justify-evenly my-4">
+                <h2 className="text-3xl">All Users</h2>
+                <h2 className="text-3xl">Total Users: {users.length}</h2>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Make Admin</th>
+                            <th>Ban User</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                  {user.role==='admin' ?'Admin' :<button
+                                        onClick={() => handleMakeAdmin(user)}
+                                        className="btn btn-lg bg-orange-500">
+                                        <FaUsers className="text-white 
+                                        text-2xl"></FaUsers>
+                                    </button>}
+                                </td>
+                                <td>
+                                    <button className="btn btn-primary">Make Admin</button>
+                                </td>
+                                <td>
+                                   <button
+                                        onClick={() => handleDeleteUser(user)}
+                                        className="btn btn-ghost btn-lg">
+                                        <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                                    </button>
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
